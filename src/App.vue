42dbbea071286fadafb79b7c8a41e70742be7ab0
aureliasegarra <av.segarra@gmail.com>
2021-11-26 21:30:17 +0100

    :recycle: Create Card component with value prop

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,33 @@
   
   <h1>Memory Game</h1>
   <section class="game-board">
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
+    <Card 
+      v-for="(card, index) in cardList"
+      :value="card"
+      :key="`card-${index}`"
+      />
 
   </section>
 </template>
 
 <script>
+import Card from "./components/Card.vue";
 
 
 export default {
   name: 'App',
   components: {
-    
+    Card,
+  },
+  setup(){
+    const cardList = [];
+
+    for (let i = 0; i < 16; i++) {
+      cardList.push(i);
+    }
+    return {
+      cardList
+    }
   }
 }
 </script>
